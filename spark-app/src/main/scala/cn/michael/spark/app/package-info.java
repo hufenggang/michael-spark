59cb4fe94c595ef442a8f0028dda78f90c2f4578
胡峰刚 <hufenggang@accesscorporate.com.cn>
2021-09-24 10:13:32 +0800

    Init commit

---
+++ spark-app/src/main/scala/cn/michael/spark/app/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by hufenggang on 2021/9/24.
+ */
+package cn.michael.spark.app;
--- spark-ml/src/main/scala/cn/michael/spark/ml/basic/statistics/CorrelationExample.scala
+++ spark-ml/src/main/scala/cn/michael/spark/ml/basic/statistics/CorrelationExample.scala
@@ -30,7 +30,7 @@ object CorrelationExample {
 
     val data = Seq(
       Vectors.sparse(4, Seq((0, 1.0), (3, -2.0))),
-      Vectors.dense(4.0, 5.0, 0.0, 3.,0),
+      Vectors.dense(4.0, 5.0, 0.0, 3.0),
       Vectors.dense(5.0, 7.0, 0.0, 8.0),
       Vectors.sparse(4, Seq((0, 9.0), (3, 1.0)))
     )
